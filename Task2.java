ef79a20c8485c7d2e9652c21454ee40f6e940cde
Elson Marqueses <77608446+pugonggubat@users.noreply.github.com>
2025-08-28 13:01:16 +0000

    push

--- Task2.java
+++ Task2.java
@@ -1,1 +1,17 @@
+public class Task2 {
 
+    public static void main(String[] args) {
+        char letter_h = 'H';
+        short num_1 = 3110;
+        char letter_w = 'w';
+        byte zero=0;
+        char letter_r='r';
+        byte one = 1;
+        char letter_d = 'd';
+        float num_2 = 2.0f;
+        boolean bool = true;
+        System.out.println(letter_h);
+        System.out.println(""+ letter_h + num_1 + " " + letter_w + zero + letter_r + one + letter_d + " " +num_2+ " "+ bool );
+
+    }
+}
