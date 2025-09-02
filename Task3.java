e8aae504a44a31b29d2a0fa022caab3bfef7caf7
Elson Marqueses <77608446+pugonggubat@users.noreply.github.com>
2025-09-02 12:26:09 +0000

    task3

--- Task3.java
+++ Task3.java
@@ -2,13 +2,14 @@ public class Task3 {
 
     public static void main(String[] args) {
         String a = new String("Wow");
-        String b = "Wow";
-        String c = a;
+        String b = a;
+        String c = "Wow!";
         String d = c;
 
         boolean b1 = a == b;
         boolean b2 = d.equals(b + "!");
-        boolean b3 = !c.equals(a)
+        boolean b3 = !c.equals(a);
+
 
 
             if (b1&&b2&&b3) {
