f3e213cd942fbcc86586c6b95f636fb09e45c728
Elson <elsonmarqueses@gmail.com>
2025-09-05 08:27:08 +0800

    task4

--- Task4.java
+++ Task4.java
@@ -9,13 +9,8 @@ public class Task4 {
         word = input.nextLine();
         System.out.println(word);
 
-        StringBuilder reverse_string = new StringBuilder();
-        int l = word.length()-1;
+        StringBuilder reverse_string = new StringBuilder(word).reverse();
 
-        for (int i=l;i>=0;i--){
-            reverse_string.append(word.charAt(i));
-//            System.out.println(reverse_string);
-        }
         boolean palindrome = word.equalsIgnoreCase(reverse_string.toString());
         System.out.println("The reverse word of your input is: " + reverse_string);
 
