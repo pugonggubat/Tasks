f717aa0ae4f0c851d5f1a2997a34a9b913444497
Elson <elsonmarqueses@gmail.com>
2025-09-06 13:59:46 +0800

    task5

--- Task5.java
+++ Task5.java
@@ -4,15 +4,27 @@ public class Task5 {
     public static void main(String[] args) {
         String[] order = {"first","second", "third"};
         int[] num = new int[3];
-
-        for(int i=0; i<=2; i++)
+        int highest = 0;
+        boolean allEqual = true;
+        for(int i=0; i<order.length; i++)
         {
             System.out.println("Enter the "+ order[i] +" number.");
             Scanner input = new Scanner(System.in);
             num[i] = Integer.parseInt(input.nextLine());
         }
         Arrays.sort(num);
-        int highest = num[num.length - 1];
-        System.out.println("The highest number among three inputs is: "+ highest);
+        for(int x=1; x < num.length; x++)
+        {
+            if (num[x] != num[0])
+            {
+                highest = num[num.length - 1];
+                allEqual=false;
+                break;
+            }
+
+        }
+
+        System.out.println(allEqual ?"All inputs are equal" : "The highest number among three inputs is: "+ highest);
+
     }
 }
