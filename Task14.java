f61ddbccbd77f53e0bd9fa44a09c06f919db436a
Elson <elsonmarqueses@gmail.com>
2025-09-29 11:50:40 +0800

    task14

---
+++ Task14.java
@@ -0,0 +1,30 @@
+abstract class abstractClass{
+    void concreteMethod(){
+        System.out.println("This is a concrete method.");
+    }
+}
+
+class B extends abstractClass{
+    void abstractMethod(){
+        System.out.println("B's iplementation of abstractMethod");
+    }
+}
+
+class C extends abstractClass{
+    void abstractMethod(){
+        System.out.println("C's implementation of abstractMethod");
+    }
+
+}
+
+public class Task14 {
+    public static void main(String[] args) {
+        B b = new B();
+        b.abstractMethod();
+        b.concreteMethod();
+        C c = new C();
+        c.abstractMethod();
+        c.concreteMethod();
+
+    }
+}
